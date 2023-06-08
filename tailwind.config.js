3b35bb528dc1506dc5ec97d4a8cf41deb5a643f3
Zhanmukhamet Yermukhamet <z.yermukhamet@outlook.com>
2023-06-08 13:18:43 +0600

    0806

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#2CB45D",
+        "weather-secondary": "#028431",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
+
